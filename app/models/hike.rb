46ac04d3381d57f3a71402dbad90f72a8f592dea
Wade Naughton <wadenaughton@gmail.com>
2023-03-29 08:44:07 -0400

    setup complete

---
+++ app/models/hike.rb
@@ -0,0 +1,15 @@
+require 'date'
+
+class Hike < ApplicationRecord
+
+  validates_presence_of :name
+  validates_presence_of :elevation
+  validates_presence_of :prominence
+  validates_presence_of :state
+  validates_presence_of :location
+  validates_presence_of :range
+  validates_presence_of :features
+
+ 
+
+end
