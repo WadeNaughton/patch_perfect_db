b60841e122c1b1753ffcaa7d25ef940de9984434
Wade Naughton <wadenaughton@gmail.com>
2023-07-25 16:47:59 -0400

    revert

--- app/controllers/api/v1/hikes_controller.rb
+++ app/controllers/api/v1/hikes_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::HikesController < ApplicationController
         @hikes = Hike.find_hike(params[:search])
         # render json: HikeSerializer.new(@hikes)
         if !@hikes.any?
-            render json: {error: "Hike not found"}
+            render json: {error: "Hike not found"}, status: 404
 
         else 
             render json: HikeSerializer.new(@hikes), status: 200
